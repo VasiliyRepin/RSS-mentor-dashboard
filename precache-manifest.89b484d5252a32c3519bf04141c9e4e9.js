84ac2e53d7faff263e19fd4437b7c3161cd07f0d
VasiliyRepin <primerior@gmail.com>
2019-02-26 03:20:31 +0300

    feat: add build packcage of task

---
+++ precache-manifest.89b484d5252a32c3519bf04141c9e4e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5e696bbe849b92b53d8f",
+    "url": "/static/js/main.5e696bbe.chunk.js"
+  },
+  {
+    "revision": "683ee795f3418d7a3c7d",
+    "url": "/static/js/2.683ee795.chunk.js"
+  },
+  {
+    "revision": "5e696bbe849b92b53d8f",
+    "url": "/static/css/main.1feaa065.chunk.css"
+  },
+  {
+    "revision": "c8def2f1f702a2fe9b00512947cd4a19",
+    "url": "/index.html"
+  }
+];
